b0d576213a6b1c27c10d9ad5f0759f853f4312b0
Christoph Reiter <reiter.christoph@gmail.com>
2022-11-12 23:11:44 +0100

    Merge the two installer packages
    It's not clear to me why there are two of them.
    Let's simplify things..

--- qt-ifw/packages/com.msys2.root/meta/installscript.js
+++ qt-ifw/packages/com.msys2.root/meta/installscript.js
@@ -26,6 +26,24 @@ function isSupported()
     return false;
 }
 
+function createShortcuts()
+{
+    var windir = installer.environmentVariable("WINDIR");
+    if (windir === "") {
+        QMessageBox["warning"]( "Error" , "Error", "Could not find windows installation directory");
+        return;
+    }
+
+    component.addOperation("CreateShortcut", "@TargetDir@/mingw32.exe", "@StartMenuDir@/MSYS2 MINGW32.lnk", "iconPath=@TargetDir@/mingw32.exe");
+    component.addOperation("CreateShortcut", "@TargetDir@/mingw64.exe", "@StartMenuDir@/MSYS2 MINGW64.lnk", "iconPath=@TargetDir@/mingw64.exe");
+    component.addOperation("CreateShortcut", "@TargetDir@/ucrt64.exe", "@StartMenuDir@/MSYS2 UCRT64.lnk", "iconPath=@TargetDir@/ucrt64.exe");
+    component.addOperation("CreateShortcut", "@TargetDir@/clang64.exe", "@StartMenuDir@/MSYS2 CLANG64.lnk", "iconPath=@TargetDir@/clang64.exe");
+    component.addOperation("CreateShortcut", "@TargetDir@/msys2.exe", "@StartMenuDir@/MSYS2 MSYS.lnk", "iconPath=@TargetDir@/msys2.exe");
+
+    component.addOperation( "Execute",
+                           ["@TargetDir@\\usr\\bin\\bash.exe", "--login", "-c", "exit"]);
+}
+
 function Component() {
 
     if (!isSupported()) {
@@ -41,4 +59,10 @@ function Component() {
     var targetDir = installer.value("TargetDir", systemDrive+"\\msys64")
 
     installer.setValue("TargetDir", targetDir);
+}
+
+Component.prototype.createOperations = function()
+{
+    component.createOperations();
+    createShortcuts();
 }
