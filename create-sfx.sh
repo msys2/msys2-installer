741fb309a289a774334d9b7db16ab834cd44741e
Christoph Reiter <reiter.christoph@gmail.com>
2023-03-18 11:12:04 +0100

    Update 7-Zip-zstd to v22.01-v1.5.4-R4
    https://github.com/mcmilk/7-Zip-zstd/releases/tag/v22.01-v1.5.4-R4

--- create-sfx.sh
+++ create-sfx.sh
@@ -4,13 +4,13 @@
 set -e
 
 # Download and extract https://github.com/mcmilk/7-Zip-zstd
-NAME="7z21.03-zstd-x64"
-CHECKSUM="531b20dfb03d8f30f61ae56a181610bbb6f3cf7cc71dac1d8f95511289de76f3"
+NAME="7z22.01-zstd-x64"
+CHECKSUM="b7fca6f44407e5fcedec98c58f3edb83407299cd88520bab7f7410d55bdd8e57"
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 mkdir -p "$DIR/_cache"
 BASE="$DIR/_cache/$NAME"
 if [ ! -f "$BASE.exe" ]; then
-    curl --fail -L "https://github.com/mcmilk/7-Zip-zstd/releases/download/v21.03-v1.5.0-R2/$NAME.exe" -o "$BASE.exe"
+    curl --fail -L "https://github.com/mcmilk/7-Zip-zstd/releases/download/v22.01-v1.5.4-R4/$NAME.exe" -o "$BASE.exe"
 fi
 echo "$CHECKSUM $BASE.exe" | sha256sum --quiet --check
 if [ ! -d "$BASE" ]; then
