2473ff0808348c290631f931d14a9c05b28be6bb
Christoph Reiter <reiter.christoph@gmail.com>
2025-08-22 11:07:47 +0200

    Update 7-Zip-zstd
    https://github.com/mcmilk/7-Zip-zstd/releases/tag/v25.01-v1.5.7-R1

--- create-sfx.sh
+++ create-sfx.sh
@@ -4,13 +4,13 @@
 set -e
 
 # Download and extract https://github.com/mcmilk/7-Zip-zstd
-NAME="7z24.09-zstd-x64"
-CHECKSUM="2378901c99005aed525971e6d308a3a80807efc6bb21075df53bbb7cd697572b"
+NAME="7z25.01-zstd-x64"
+CHECKSUM="d140094f0277b49a4e895159bd734da03cd2b60fb73a65e4151edfedc612981e"
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 mkdir -p "$DIR/_cache"
 BASE="$DIR/_cache/$NAME"
 if [ ! -f "$BASE.exe" ]; then
-    curl --fail -L "https://github.com/mcmilk/7-Zip-zstd/releases/download/v24.09-v1.5.7-R1/$NAME.exe" -o "$BASE.exe"
+    curl --fail -L "https://github.com/mcmilk/7-Zip-zstd/releases/download/v25.01-v1.5.7-R1/$NAME.exe" -o "$BASE.exe"
 fi
 echo "$CHECKSUM $BASE.exe" | sha256sum --quiet --check
 if [ ! -d "$BASE" ]; then
